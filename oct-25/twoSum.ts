26002ad5ceaf41599a2be4e7db57c1928edd4689
Susan Koju <susankoju@pingidentity.com>
2024-11-07 07:26:11 +0545

    Add two sum

---
+++ oct-25/twoSum.ts
@@ -0,0 +1,13 @@
+const findTwoSum = (arr: number[], target: number): number[] => {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length; j++) {
+            if (arr[i] + arr[j] === target) {
+                return [arr[i], arr[j]]
+            }
+        }
+    }
+
+    return [];
+}
+
+console.log(findTwoSum([2, 7, 11, 15], 9));
