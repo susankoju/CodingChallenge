1b0848ed605dac1d1cf71c6a287b4c3aefd496b9
Susan Koju <susankoju@pingidentity.com>
2024-11-07 07:28:19 +0545

    Refactor twoSum

--- oct-25/twoSum.js
+++ oct-25/twoSum.js
@@ -1,7 +1,7 @@
 "use strict";
 const findTwoSum = (arr, target) => {
-    for (let i = 0; i < arr.length; i++) {
-        for (let j = 0; j < arr.length; j++) {
+    for (let i = 0; i < arr.length - 1; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
             if (arr[i] + arr[j] === target) {
                 return [arr[i], arr[j]];
             }
@@ -10,3 +10,6 @@ const findTwoSum = (arr, target) => {
     return [];
 };
 console.log(findTwoSum([2, 7, 11, 15], 9));
+console.log(findTwoSum([11, 15, 2, 7], 9));
+console.log(findTwoSum([11, 15, 2, 7, 1], 9));
+console.log(findTwoSum([11, 15, 1, 7, 1], 9));
