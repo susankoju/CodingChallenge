2c86d9919436d33462315d5a02115584b8706f64
Susan Koju <susankoju@pingidentity.com>
2024-11-08 11:21:37 +0545

    Update merge string alternatingly

--- oct-25/mergeStringAlternatingly.js
+++ oct-25/mergeStringAlternatingly.js
@@ -1,1 +1,21 @@
 "use strict";
+const mergeStringAlternatingly = (str1, str2) => {
+    let mergedStr = '';
+    let shorterStr = '';
+    let largerStr = '';
+    if (str1.length > str2.length) {
+        shorterStr = str2;
+        largerStr = str1;
+    }
+    else {
+        shorterStr = str1;
+        largerStr = str2;
+    }
+    for (let i = 0; i < shorterStr.length; i++) {
+        mergedStr += (str1.charAt(i) + str2.charAt(i));
+    }
+    return mergedStr + largerStr.slice(shorterStr.length);
+};
+console.log(mergeStringAlternatingly('abc', 'pqr'));
+console.log(mergeStringAlternatingly('ab', 'pqrs'));
+console.log(mergeStringAlternatingly('abcd', 'pq'));
--- oct-25/mergeStringAlternatingly.ts
+++ oct-25/mergeStringAlternatingly.ts
@@ -1,5 +1,21 @@
-const mergeStringAlternatingly = (str1: string, str2: string) => {
+const mergeStringAlternatingly = (str1: string, str2: string): string => {
+    let mergedStr = '';
+    let shorterStr = '';
+    let largerStr = '';
+    if (str1.length > str2.length) {
+        shorterStr = str2;
+        largerStr = str1;
+    } else {
+        shorterStr = str1;
+        largerStr = str2;
+    }
+    for (let i = 0; i < shorterStr.length; i++) {
+        mergedStr += (str1.charAt(i) + str2.charAt(i));
+    }
 
+    return mergedStr + largerStr.slice(shorterStr.length);
 }
 
+console.log(mergeStringAlternatingly('abc', 'pqr'))
 console.log(mergeStringAlternatingly('ab', 'pqrs'))
+console.log(mergeStringAlternatingly('abcd', 'pq'))
