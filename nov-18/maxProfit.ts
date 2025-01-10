83aed7377d1fc1637e2069181119154794e1673f
Susan Koju <susankoju@pingidentity.com>
2025-01-10 09:20:26 +0545

    Add max profit possible

---
+++ nov-18/maxProfit.ts
@@ -0,0 +1,14 @@
+const getMaxProfitPossible = (prices: number[]): number => {
+  let maxProfit = 0;
+  for (let i = 0; i < prices.length; i++) {
+    for (let j = i + 1; j < prices.length; j++) {
+      if (prices[j] - prices[i] > maxProfit) {
+        maxProfit = prices[j] - prices[i];
+      }
+    }
+  }
+  return maxProfit;
+}
+
+console.log(getMaxProfitPossible([7,1,5,3,6,4]));
+console.log(getMaxProfitPossible([7,6,4,3,1]));
