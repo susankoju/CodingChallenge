79f3aa4af514af07714134c7a68427c6c061a253
Susan Koju <susankoju@pingidentity.com>
2025-01-10 09:39:04 +0545

    Add majority element

---
+++ nov-18/majorityElement.js
@@ -0,0 +1,18 @@
+"use strict";
+const findMajorityElement = (arr) => {
+    let majorityElem = arr[0];
+    let maxCount = 0;
+    for (let i = 1; i < arr.length; i++) {
+        let count = 0;
+        if (arr[i] === majorityElem) {
+            count++;
+        }
+        if (count > maxCount) {
+            majorityElem = arr[i];
+            maxCount = count;
+        }
+    }
+    return majorityElem;
+};
+console.log(findMajorityElement([3, 2, 3]));
+console.log(findMajorityElement([2, 2, 1, 1, 1, 2, 2]));
