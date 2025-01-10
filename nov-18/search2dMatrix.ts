e743ce6ea726d3b320eb973278c040ec84409969
Susan Koju <susankoju@pingidentity.com>
2025-01-10 07:12:23 +0545

    Add 2d matrix search

---
+++ nov-18/search2dMatrix.ts
@@ -0,0 +1,9 @@
+const isInArray = (num: number, arr: number[][]) => {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].includes(num)) return true;
+  }
+  return false;
+}
+
+console.log(isInArray(3, [[1,3,5,7],[10,11,16,20],[23,30,34,60]]));
+console.log(isInArray(13, [[1,3,5,7],[10,11,16,20],[23,30,34,60]]));
